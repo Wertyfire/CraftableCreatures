a1429f88067feba9734cb5145e34d50717341573
Wertyfire <sw405@yandex.ru>
2023-11-18 19:23:07 +0300

    Удалена переменная i1

--- src/main/java/ru/wertyfiregames/craftablecreatures/transmutator/inventory/GuiTransmutator.java
+++ src/main/java/ru/wertyfiregames/craftablecreatures/transmutator/inventory/GuiTransmutator.java
@@ -56,7 +56,6 @@ public class GuiTransmutator extends GuiContainer
         int k = (this.width - this.xSize) / 2;
         int l = (this.height - this.ySize) / 2;
         this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.ySize);
-        int i1;
         drawPlayerModel(k + 51, l + 75, 27, (float) (k + 51) - this.xSize_lo, (float) (l + 75 - 50) - this.ySize_lo, this.mc.thePlayer);
     }
 
