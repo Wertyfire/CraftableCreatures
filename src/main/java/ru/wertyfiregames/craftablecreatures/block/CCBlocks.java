c0ee4296122683a0ab4790901e7e9000a1a5a132
Wertyfire <sw405@yandex.ru>
2024-02-01 22:12:16 +0300

    Окончательно (для этой версии) удалён извлекатель душ

--- src/main/java/ru/wertyfiregames/craftablecreatures/block/CCBlocks.java
+++ src/main/java/ru/wertyfiregames/craftablecreatures/block/CCBlocks.java
@@ -1,7 +1,6 @@
 package ru.wertyfiregames.craftablecreatures.block;
 
 import cpw.mods.fml.common.registry.GameRegistry;
-import ru.wertyfiregames.craftablecreatures.common.config.CCConfigHandler;
 import ru.wertyfiregames.craftablecreatures.creativetab.CCCreativeTabs;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.MapColor;
@@ -16,16 +15,11 @@ public class CCBlocks
             CCCreativeTabs.tabCraftableCreatures, "pickaxe", 2, 5f, 10f);
     public static final Block bluestone_ore = new BlockCCCompressed(MapColor.stoneColor, Material.rock, "bluestoneOre", "bluestone_ore",
             CCCreativeTabs.tabCraftableCreatures, "pickaxe", 2, 3f, 5f);
-    public static final Block soulExtractor = new BlockSE();
 
     public static void register()
     {
         GameRegistry.registerBlock(powered_bluestone_block, "powered_bluestone_block");
         GameRegistry.registerBlock(bluestone_block, "bluestone_block");
         GameRegistry.registerBlock(bluestone_ore, "bluestone_ore");
-        if (CCConfigHandler.enableExperimentalContent)
-        {
-            GameRegistry.registerBlock(soulExtractor, "soul_extractor");
-        }
     }
 }
--- src/main/java/ru/wertyfiregames/craftablecreatures/proxy/CommonProxy.java
+++ src/main/java/ru/wertyfiregames/craftablecreatures/proxy/CommonProxy.java
@@ -4,20 +4,20 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.IGuiHandler;
+import cpw.mods.fml.common.network.NetworkRegistry;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 import ru.wertyfiregames.craftablecreatures.CraftableCreatures;
 
 public class CommonProxy implements IGuiHandler
 {
-
     public void preInit(FMLPreInitializationEvent event)
     {
 
     }
     public void init(FMLInitializationEvent event)
     {
-
+        NetworkRegistry.INSTANCE.registerGuiHandler(CraftableCreatures.INSTANCE, this);
     }
     public void postInit(FMLPostInitializationEvent event)
     {
@@ -27,22 +27,12 @@ public class CommonProxy implements IGuiHandler
     @Override
     public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
     {
-        if (ID == CraftableCreatures.GUI_TRANSMUTATOR)
-        {
-            return null;
-        }
-
         return null;
     }
 
     @Override
     public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
     {
-        if (ID == CraftableCreatures.GUI_TRANSMUTATOR)
-        {
-            return null;
-        }
-
         return null;
     }
 }
