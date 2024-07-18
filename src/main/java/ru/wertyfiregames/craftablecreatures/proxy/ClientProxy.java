de97eabe8fdbf5a8334123b18e034f958842d12a
Wertyfire <sw405@yandex.ru>
2024-07-18 13:02:44 +0300

    Улучшена система генерации руд

--- src/main/java/ru/wertyfiregames/craftablecreatures/proxy/ClientProxy.java
+++ src/main/java/ru/wertyfiregames/craftablecreatures/proxy/ClientProxy.java
@@ -4,36 +4,31 @@ import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.VillagerRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import ru.wertyfiregames.craftablecreatures.CraftableCreatures;
 import ru.wertyfiregames.craftablecreatures.block.CCBlocks;
-import ru.wertyfiregames.craftablecreatures.common.config.CCConfigHandler;
+import ru.wertyfiregames.craftablecreatures.config.CCConfig;
 import ru.wertyfiregames.craftablecreatures.common.handler.TradeHandler;
 import ru.wertyfiregames.craftablecreatures.common.listener.CCEventListener;
 import ru.wertyfiregames.craftablecreatures.item.CCItems;
 import ru.wertyfiregames.craftablecreatures.recipe.CCOreDictionary;
-import ru.wertyfiregames.craftablecreatures.render.CCRenderers;
 import ru.wertyfiregames.craftablecreatures.recipe.CCRecipes;
 import ru.wertyfiregames.craftablecreatures.stats.CCAchievementList;
-import ru.wertyfiregames.craftablecreatures.world.CCWorldGenVines;
+import ru.wertyfiregames.craftablecreatures.world.CCWorldOreGenerator;
 
 @SideOnly(Side.CLIENT)
 public class ClientProxy extends CommonProxy {
     public void preInit(FMLPreInitializationEvent event) {
-        CraftableCreatures.configDir = event.getModConfigurationDirectory().toString();
-        CCConfigHandler.register(CraftableCreatures.configDir);
-        FMLCommonHandler.instance().bus().register(new CCConfigHandler());
+        FMLCommonHandler.instance().bus().register(new CCConfig());
         FMLCommonHandler.instance().bus().register(new CCEventListener());
         CraftableCreatures.getModLogger().debug("CC Config loaded");
-
         CCItems.register();
         CraftableCreatures.getModLogger().debug("CC Items loaded");
         CCBlocks.register();
         CraftableCreatures.getModLogger().debug("CC Blocks loaded");
-        CCWorldGenVines.register();
-        CraftableCreatures.getModLogger().debug("CC Ore generation loaded");
         CCAchievementList.register();
         CraftableCreatures.getModLogger().debug("CC Achievements loaded");
 
@@ -46,16 +41,16 @@ public class ClientProxy extends CommonProxy {
         CraftableCreatures.getModLogger().info("Pre initialization of Craftable Creatures complete");
     }
     public void init(FMLInitializationEvent event) {
-        CCRenderers.registerItemRenderers();
-        CraftableCreatures.getModLogger().debug("CC Item renderers loaded");
-        super.init(event);
-        CraftableCreatures.getModLogger().info("Initialization of Craftable Creatures complete");
-    }
-    public void postInit(FMLPostInitializationEvent event) {
+        GameRegistry.registerWorldGenerator(new CCWorldOreGenerator(), 0);
+        CraftableCreatures.getModLogger().debug("CC Ore generation loaded");
         CCOreDictionary.register();
         CraftableCreatures.getModLogger().debug("CC Recipes loaded");
         CCRecipes.register();
         CraftableCreatures.getModLogger().debug("CC Ore dictionary loaded");
+        super.init(event);
+        CraftableCreatures.getModLogger().info("Initialization of Craftable Creatures complete");
+    }
+    public void postInit(FMLPostInitializationEvent event) {
         super.postInit(event);
         CraftableCreatures.getModLogger().info("Post initialization of Craftable Creatures complete");
     }
---
+++ src/main/java/ru/wertyfiregames/craftablecreatures/world/CCWorldOreGenerator.java
@@ -0,0 +1,40 @@
+package ru.wertyfiregames.craftablecreatures.world;
+
+import cpw.mods.fml.common.IWorldGenerator;
+import net.minecraft.block.Block;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.feature.WorldGenMinable;
+import ru.wertyfiregames.craftablecreatures.block.CCBlocks;
+
+import java.util.Random;
+
+public class CCWorldOreGenerator implements IWorldGenerator
+{
+    @Override
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+        switch (world.provider.dimensionId) {
+            case -1:
+                generateNether(world, random, chunkX * 16, chunkZ * 16);
+            case 0:
+                generateSurface(world, random, chunkX * 16, chunkZ * 16);
+            case 1:
+                generateEnd(world, random, chunkX * 16, chunkZ * 16);
+        }
+    }
+
+    private void generateNether(World world, Random random, int x, int z) {}
+    private void generateSurface(World world, Random random, int x, int z) {
+        addOreSpawn(CCBlocks.bluestone_ore, world, random, x, z, 16, 16, 8, 12, 20, 60);
+    }
+    private void generateEnd(World world, Random random, int x, int z) {}
+
+    private void addOreSpawn(Block block, World world, Random random, int blockXPos, int blockZPos, int maxX, int maxZ, int maxVeinSize, int chancesToSpawn, int minY, int maxY) {
+        for (int i = 0; i < chancesToSpawn; i++) {
+            int posX = blockXPos + random.nextInt(maxX);
+            int posY = minY + random.nextInt(maxY - minY);
+            int posZ = blockZPos + random.nextInt(maxZ);
+            new WorldGenMinable(block, maxVeinSize).generate(world, random, posX, posY, posZ);
+        }
+    }
+}
