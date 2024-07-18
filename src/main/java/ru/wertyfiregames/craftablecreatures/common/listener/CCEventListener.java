f365b7e58a1916223941728dab47679050cfd9d9
Wertyfire <sw405@yandex.ru>
2024-07-18 13:37:03 +0300

    Улучшена система проверки версий и выдачи достижений за создание предмета

--- src/main/java/ru/wertyfiregames/craftablecreatures/common/listener/CCEventListener.java
+++ src/main/java/ru/wertyfiregames/craftablecreatures/common/listener/CCEventListener.java
@@ -2,19 +2,21 @@ package ru.wertyfiregames.craftablecreatures.common.listener;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatComponentText;
 import ru.wertyfiregames.craftablecreatures.CraftableCreatures;
 import ru.wertyfiregames.craftablecreatures.block.CCBlocks;
-import ru.wertyfiregames.craftablecreatures.common.config.CCConfigHandler;
+import ru.wertyfiregames.craftablecreatures.config.CCConfig;
 import ru.wertyfiregames.craftablecreatures.item.CCItems;
 import ru.wertyfiregames.craftablecreatures.stats.CCAchievementList;
-import ru.wertyfiregames.craftablecreatures.version.CCVersion;
-import ru.wertyfiregames.craftablecreatures.version.CCVersion.UpdateResult;
+import ru.wertyfiregames.craftablecreatures.util.Utils;
+import ru.wertyfiregames.craftablecreatures.version.CCVersionChecker;
+import ru.wertyfiregames.craftablecreatures.version.CCVersionChecker.UpdateResult;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 
 public class CCEventListener {
     @SubscribeEvent
@@ -25,61 +27,61 @@ public class CCEventListener {
     @SubscribeEvent
     public void onPlayerLoggedInEvent(PlayerEvent.PlayerLoggedInEvent event) {
         event.player.triggerAchievement(CCAchievementList.thanksForDownload);
-        event.player.addChatMessage(new ChatComponentText(I18n.format("chat.craftableCreatures.modInfo") + " " + CCConfigHandler.enableExperimentalContent));
+        event.player.addChatMessage(new ChatComponentText(I18n.format("chat.craftableCreatures.modInfo") + CCConfig.enableExperimentalContent));
 
-        if (CCConfigHandler.checkForUpdates) {
-            try {
-                URL link = new URL("https://modrinth.com/mod/craftable_creatures");
-
-                if (CCVersion.getStatus() == UpdateResult.FAILED) {
-                    event.player.addChatMessage(new ChatComponentText(I18n.format("chat.craftableCreatures.failedToCheckUpdates")));
+        if (CCConfig.checkForUpdates) {
+            String homepage = CCVersionChecker.getHomepageUrl();
+            if (CCVersionChecker.getStatus() == UpdateResult.FAILED) {
+                event.player.addChatMessage(new ChatComponentText(I18n.format("chat.craftableCreatures.failedToCheckUpdates")));
+            }
+            if (CCVersionChecker.getStatus() == UpdateResult.UP_TO_DATE) {
+                event.player.addChatMessage(new ChatComponentText(I18n.format("chat.craftableCreatures.latest")));
+            }
+            if (CCVersionChecker.getStatus() == UpdateResult.OUTDATED) {
+                event.player.addChatMessage(new ChatComponentText(I18n.format("chat.craftableCreatures.outdated")));
+                Utils.sendClickableLink(event.player, "chat.craftableCreatures.getUpdate", homepage, homepage, "");
+                for (String change : CCVersionChecker.getChangelog().split("<n>")) {
+                    if (change.contains("<t>"))
+                        change = change.replace("<t>", "    ");
+                    event.player.addChatMessage(new ChatComponentText(change));
                 }
-                if (CCVersion.getStatus() == UpdateResult.UP_TO_DATE) {
-                    event.player.addChatMessage(new ChatComponentText(I18n.format("chat.craftableCreatures.latest")));
-                }
-                if (CCVersion.getStatus() == UpdateResult.OUTDATED) {
-                    event.player.addChatMessage(new ChatComponentText(I18n.format("chat.craftableCreatures.outdated")));
-                    event.player.addChatMessage(new ChatComponentText(I18n.format("chat.craftableCreatures.getUpdate") + " " + link));
-                }
-                if (CCVersion.getStatus() == UpdateResult.BETA_OUTDATED) {
-                    event.player.addChatMessage(new ChatComponentText(I18n.format("chat.craftableCreatures.betaOutdated")));
-                    event.player.addChatMessage(new ChatComponentText(I18n.format("chat.craftableCreatures.getUpdate") + " " + link));
-                }
-            } catch (MalformedURLException e) {
-                CraftableCreatures.getModLogger().error("Failed to create update URL!");
-
-                if (CCVersion.getStatus() == UpdateResult.FAILED) {
-                    event.player.addChatMessage(new ChatComponentText(I18n.format("chat.craftableCreatures.failedToCheckUpdates")));
-                }
-                if (CCVersion.getStatus() == UpdateResult.UP_TO_DATE) {
-                    event.player.addChatMessage(new ChatComponentText(I18n.format("chat.craftableCreatures.latest")));
-                }
-                if (CCVersion.getStatus() == UpdateResult.OUTDATED) {
-                    event.player.addChatMessage(new ChatComponentText(I18n.format("chat.craftableCreatures.outdated")));
-                    event.player.addChatMessage(new ChatComponentText(I18n.format("chat.craftableCreatures.getUpdate") + " https://modrinth.com/mod/craftable_creatures"));
-                }
-                if (CCVersion.getStatus() == UpdateResult.BETA_OUTDATED) {
-                    event.player.addChatMessage(new ChatComponentText(I18n.format("chat.craftableCreatures.betaOutdated")));
-                    event.player.addChatMessage(new ChatComponentText(I18n.format("chat.craftableCreatures.getUpdate") + " https://modrinth.com/mod/craftable_creatures"));
+            }
+            if (CCVersionChecker.getStatus() == UpdateResult.AHEAD) {
+                event.player.addChatMessage(new ChatComponentText(I18n.format("chat.craftableCreatures.ahead")));
+            }
+            if (CCVersionChecker.getStatus() == UpdateResult.BETA) {
+                event.player.addChatMessage(new ChatComponentText(I18n.format("chat.craftableCreatures.beta")));
+            }
+            if (CCVersionChecker.getStatus() == UpdateResult.BETA_OUTDATED) {
+                event.player.addChatMessage(new ChatComponentText(I18n.format("chat.craftableCreatures.betaOutdated")));
+                Utils.sendClickableLink(event.player, "chat.craftableCreatures.getUpdate", homepage, homepage, "");
+                for (String change : CCVersionChecker.getChangelog().split("<n>")) {
+                    if (change.contains("<t>"))
+                        change = change.replace("<t>", "    ");
+                    event.player.addChatMessage(new ChatComponentText(change));
                 }
             }
         }
     }
 
-    public void onItemCrafted(PlayerEvent.ItemCraftedEvent event)
-    {
-        if (event.player != null) {
-            if (event.crafting.getItem() == CCItems.bluestone) {
-                event.player.triggerAchievement(CCAchievementList.bluedustCollector);
-            }
-            if (event.crafting.getItem() == CCItems.template) {
-                event.player.triggerAchievement(CCAchievementList.templateManager);
-            }
-            if (event.crafting.getItem() == CCItems.spawn_egg_template) {
-                event.player.triggerAchievement(CCAchievementList.mobSpawner);
-            }
-            if (event.crafting.getItem() == Item.getItemFromBlock(CCBlocks.powered_bluestone_block)) {
-                event.player.triggerAchievement(CCAchievementList.energy);
+    @SubscribeEvent
+    public void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        if (event.phase == TickEvent.Phase.END && event.player != null) {
+            for (ItemStack stack : event.player.inventory.mainInventory) {
+                if (stack != null) {
+                    if (stack.getItem() == CCItems.bluestone) {
+                        event.player.triggerAchievement(CCAchievementList.bluedustCollector);
+                    }
+                    if (stack.getItem() == CCItems.template) {
+                        event.player.triggerAchievement(CCAchievementList.templateManager);
+                    }
+                    if (stack.getItem() == CCItems.spawn_egg_template) {
+                        event.player.triggerAchievement(CCAchievementList.mobSpawner);
+                    }
+                    if (stack.getItem() == Item.getItemFromBlock(CCBlocks.powered_bluestone_block)) {
+                        event.player.triggerAchievement(CCAchievementList.energy);
+                    }
+                }
             }
         }
     }
