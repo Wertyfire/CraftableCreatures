e104ad1ac5b9ce25c473a4891f44c915384cab6d
Wertyfire <sw405@yandex.ru>
2024-07-19 12:51:55 +0300

    Словарь руд перемещён в пакет compat

--- src/main/java/ru/wertyfiregames/craftablecreatures/compat/CCOreDictionary.java
+++ src/main/java/ru/wertyfiregames/craftablecreatures/compat/CCOreDictionary.java
@@ -2,16 +2,14 @@
  * File created on 17:20 20.11.2023 by Wertyfire
  */
 
-package ru.wertyfiregames.craftablecreatures.recipe;
+package ru.wertyfiregames.craftablecreatures.compat;
 
 import net.minecraftforge.oredict.OreDictionary;
-import ru.wertyfiregames.craftablecreatures.block.CCBlocks;
-import ru.wertyfiregames.craftablecreatures.item.CCItems;
+import ru.wertyfiregames.craftablecreatures.init.CCBlocks;
+import ru.wertyfiregames.craftablecreatures.init.CCItems;
 
-public class CCOreDictionary
-{
-    public static void register()
-    {
+public class CCOreDictionary {
+    public static void register() {
 //        Ores
         OreDictionary.registerOre("oreBluestone", CCBlocks.bluestone_ore);
 
