cc5ecbc3c6b8c5944ea5d6f231a68f873011f2d8
Wertyfire <sw405@yandex.ru>
2024-07-20 15:40:29 +0300

    Перемещён из listener\

--- src/main/java/ru/wertyfiregames/craftablecreatures/common/CCEventListener.java
+++ src/main/java/ru/wertyfiregames/craftablecreatures/common/CCEventListener.java
@@ -1,4 +1,4 @@
-package ru.wertyfiregames.craftablecreatures.common.listener;
+package ru.wertyfiregames.craftablecreatures.common;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
