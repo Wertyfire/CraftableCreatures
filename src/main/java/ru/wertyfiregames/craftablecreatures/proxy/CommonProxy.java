141dbe58abfe25cae1136d84ebe456e6c5b281e1
Wertyfire <sw405@yandex.ru>
2024-08-04 22:27:12 +0300

    Изменена система прокси

--- src/main/java/ru/wertyfiregames/craftablecreatures/proxy/CommonProxy.java
+++ src/main/java/ru/wertyfiregames/craftablecreatures/proxy/CommonProxy.java
@@ -1,38 +1,28 @@
+/**
+ * File created on 16:45 01.08.2024 by Wertyfire
+ */
+
 package ru.wertyfiregames.craftablecreatures.proxy;
 
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.IGuiHandler;
-import cpw.mods.fml.common.network.NetworkRegistry;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 import ru.wertyfiregames.craftablecreatures.CraftableCreatures;
+import ru.wertyfiregames.craftablecreatures.inventory.gui.GuiSoulExtractor;
+import ru.wertyfiregames.craftablecreatures.inventory.container.ContainerSoulExtractor;
+import ru.wertyfiregames.craftablecreatures.tileentity.TileEntitySoulExtractor;
 
-public class CommonProxy implements IGuiHandler
-{
-    public void preInit(FMLPreInitializationEvent event)
-    {
-
-    }
-    public void init(FMLInitializationEvent event)
-    {
-        NetworkRegistry.INSTANCE.registerGuiHandler(CraftableCreatures.INSTANCE, this);
-    }
-    public void postInit(FMLPostInitializationEvent event)
-    {
-
-    }
-
-    @Override
-    public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
-    {
+public class CommonProxy implements IGuiHandler {
+    public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+        if (ID == CraftableCreatures.GUI_SOUL_EXTRACTOR)
+            return new GuiSoulExtractor(player.inventory, (TileEntitySoulExtractor) world.getTileEntity(x, y, z));
         return null;
     }
 
-    @Override
-    public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
-    {
+    public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+        if (ID == CraftableCreatures.GUI_SOUL_EXTRACTOR)
+            return new ContainerSoulExtractor(player.inventory, (TileEntitySoulExtractor) world.getTileEntity(x, y, z));
+
         return null;
     }
 }
