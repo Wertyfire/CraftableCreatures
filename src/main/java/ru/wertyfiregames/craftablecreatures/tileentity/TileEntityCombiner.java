d3c25242d99b22ea044d6d375600aa84397d221e
Wertyfire <sw405@yandex.ru>
2024-09-04 20:03:42 +0300

    Удалён импорт

--- src/main/java/ru/wertyfiregames/craftablecreatures/tileentity/TileEntityCombiner.java
+++ src/main/java/ru/wertyfiregames/craftablecreatures/tileentity/TileEntityCombiner.java
@@ -6,7 +6,6 @@ package ru.wertyfiregames.craftablecreatures.tileentity;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.ISidedInventory;
 import net.minecraft.item.ItemStack;
