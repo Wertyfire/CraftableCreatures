dcfcd9298ff9637dd772f80a0619f4e441569eed
Wertyfire <sw405@yandex.ru>
2024-09-07 12:12:05 +0300

    Добавлен тултип

--- src/main/java/ru/wertyfiregames/craftablecreatures/item/ItemGuideBook.java
+++ src/main/java/ru/wertyfiregames/craftablecreatures/item/ItemGuideBook.java
@@ -1,15 +1,24 @@
 package ru.wertyfiregames.craftablecreatures.item;
 
+import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import ru.wertyfiregames.craftablecreatures.CraftableCreatures;
 import ru.wertyfiregames.craftablecreatures.creativetab.CCCreativeTabs;
 
+import java.util.List;
+
 public class ItemGuideBook extends ItemDefault {
     public ItemGuideBook() {
         super("guideBook", "guide_book", CCCreativeTabs.tabCraftableCreatures);
         this.setMaxStackSize(1);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void addInformation(ItemStack stack, EntityPlayer player, List tooltip, boolean flag) {
+        tooltip.add(I18n.format("craftableCreatures.guide.page1.author") + " " + "Wertyfire");
     }
 
     @Override
