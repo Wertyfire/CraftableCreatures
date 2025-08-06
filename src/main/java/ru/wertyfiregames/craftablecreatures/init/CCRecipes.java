b88cd1c44046cf7fa89f6a28dcc52c639c5444cb
Wertyfire <sw405@yandex.ru>
2025-08-06 19:10:16 +0300

    Изменён крафт заряженного блока синего камня и удалены рецепты которые теперь в комбинаторе

--- src/main/java/ru/wertyfiregames/craftablecreatures/init/CCRecipes.java
+++ src/main/java/ru/wertyfiregames/craftablecreatures/init/CCRecipes.java
@@ -17,14 +17,14 @@ public class CCRecipes {
                 new ItemStack(Items.egg), new ItemStack(CCItems.template));
         GameRegistry.addShapelessRecipe(new ItemStack(CCItems.bluestone, 9),
                 new ItemStack(CCBlocks.bluestone_block));
-        GameRegistry.addShapelessRecipe(new ItemStack(CCItems.bluestone, 8),
+        GameRegistry.addShapelessRecipe(new ItemStack(CCItems.bluestone, 4),
                 new ItemStack(CCBlocks.powered_bluestone_block));
 
         //Shaped recipes
         GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(CCItems.template), true,
                 "SR", "RS", 'R', "dustBluestone", 'S', Items.paper));
         GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(CCBlocks.powered_bluestone_block),
-                "SSS", "SUS", "SSS", 'S', "dustBluestone", 'U', "dustRedstone"));
+                "SUS", "UUU", "SUS", 'S', "dustBluestone", 'U', "dustRedstone"));
         GameRegistry.addShapedRecipe(new ItemStack(CCBlocks.bluestone_block),
                 "SSS", "SSS", "SSS", 'S', CCItems.bluestone);
         GameRegistry.addShapedRecipe(new ItemStack(CCItems.guide_book),
@@ -36,56 +36,5 @@ public class CCRecipes {
 
         //Smelting recipes
         GameRegistry.addSmelting(CCBlocks.bluestone_ore, new ItemStack(CCItems.bluestone), 0.7f);
-
-        if (CCConfig.enableExperimentalContent) return;
-
-        //Spawn eggs
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1,
-                EntityList.getEntityID(new EntityCreeper(null))), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 1));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1,
-                EntityList.getEntityID(new EntitySkeleton(null))), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 2));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1,
-                EntityList.getEntityID(new EntitySpider(null))), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 3));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1,
-                EntityList.getEntityID(new EntityZombie(null))), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 4));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1,
-                EntityList.getEntityID(new EntitySlime(null))), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 5));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1,
-                EntityList.getEntityID(new EntityGhast(null))), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 6));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1,
-                EntityList.getEntityID(new EntityPigZombie(null))), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 7));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1,
-                EntityList.getEntityID(new EntityEnderman(null))), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 8));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1,
-                EntityList.getEntityID(new EntityCaveSpider(null))), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 9));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1,
-                EntityList.getEntityID(new EntitySilverfish(null))), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 10));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1,
-                EntityList.getEntityID(new EntityBlaze(null))), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 11));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1,
-                EntityList.getEntityID(new EntityMagmaCube(null))), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 12));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1,
-                EntityList.getEntityID(new EntityBat(null))), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 13));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1,
-                EntityList.getEntityID(new EntityWitch(null))), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 14));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1,
-                EntityList.getEntityID(new EntityPig(null))), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 15));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1,
-                EntityList.getEntityID(new EntitySheep(null))), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 16));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1,
-                EntityList.getEntityID(new EntityCow(null))), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 17));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1,
-                EntityList.getEntityID(new EntityChicken(null))), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 18));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1,
-                EntityList.getEntityID(new EntitySquid(null))), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 19));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1,
-                EntityList.getEntityID(new EntityWolf(null))), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 20));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1,
-                EntityList.getEntityID(new EntityMooshroom(null))), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 21));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1,
-                EntityList.getEntityID(new EntityOcelot(null))), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 22));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1, 100), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 23));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.spawn_egg, 1,
-                EntityList.getEntityID(new EntityVillager(null))), new ItemStack(CCItems.spawn_egg_template), new ItemStack(CCItems.soul_element, 1, 24));
     }
 }
