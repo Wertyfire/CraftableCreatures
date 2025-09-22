0d5872de1de746188a45e7d82cc119ee31990f8d
Wertyfire <sw405@yandex.ru>
2025-09-22 17:16:00 +0300

    Теперь максимальное значение меты генерируемых душ берётся из EnumSoulElement

--- src/main/java/ru/wertyfiregames/craftablecreatures/init/CCChestLoot.java
+++ src/main/java/ru/wertyfiregames/craftablecreatures/init/CCChestLoot.java
@@ -6,11 +6,12 @@ package ru.wertyfiregames.craftablecreatures.init;
 
 import net.minecraft.util.WeightedRandomChestContent;
 import net.minecraftforge.common.ChestGenHooks;
+import ru.wertyfiregames.craftablecreatures.item.EnumSoulElement;
 
 public class CCChestLoot {
     public static void register() {
         //Souls
-        for (int i = 14; i <= 24; i++) {
+        for (int i = 14; i <= EnumSoulElement.values().length; i++) {
             ChestGenHooks.getInfo(ChestGenHooks.DUNGEON_CHEST).addItem(new WeightedRandomChestContent(CCItems.soul, i, 0, 1, 1));
             ChestGenHooks.getInfo(ChestGenHooks.MINESHAFT_CORRIDOR).addItem(new WeightedRandomChestContent(CCItems.soul, i, 1, 3, 2));
             ChestGenHooks.getInfo(ChestGenHooks.PYRAMID_DESERT_CHEST).addItem(new WeightedRandomChestContent(CCItems.soul, i, 0, 1, 1));
