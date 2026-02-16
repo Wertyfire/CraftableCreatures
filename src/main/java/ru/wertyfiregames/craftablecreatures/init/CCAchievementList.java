592460c95e05fa330aed437efc73fa56c4f1ab4e
Wertyfire <sw405@yandex.ru>
2026-02-16 15:20:20 +0300

    Достижения на экране достижений были переставлены

--- src/main/java/ru/wertyfiregames/craftablecreatures/init/CCAchievementList.java
+++ src/main/java/ru/wertyfiregames/craftablecreatures/init/CCAchievementList.java
@@ -6,26 +6,26 @@ import net.minecraftforge.common.AchievementPage;
 
 public class CCAchievementList {
     public static final Achievement thanksForDownload = new Achievement("achievement.craftableCreatures.thanksForDownload",
-            "craftableCreatures.thanksForDownload", 2, -1,
+            "craftableCreatures.thanksForDownload", -1, 0,
             CCItems.GUIDE_BOOK, null).registerStat();
     public static final Achievement bluedustCollector = new Achievement("achievement.craftableCreatures.bluedustCollector",
-            "craftableCreatures.bluedustCollector", 0, 0, CCItems.BLUESTONE, thanksForDownload).registerStat();
+            "craftableCreatures.bluedustCollector", 1, 0, CCItems.BLUESTONE, thanksForDownload).registerStat();
     public static final Achievement transmutator = new Achievement("achievement.craftableCreatures.transmutator",
-            "craftableCreatures.transmutator", -2, 0, CCItems.TRANSMUTATOR, bluedustCollector).registerStat();
+            "craftableCreatures.transmutator", 1, -2, CCItems.TRANSMUTATOR, bluedustCollector).registerStat();
     public static final Achievement templateManager = new Achievement("achievement.craftableCreatures.templateManager",
             "craftableCreatures.templateManager", 1, 2, CCItems.BLUEPRINT, bluedustCollector).registerStat();
     public static final Achievement mobSpawner = new Achievement("achievement.craftableCreatures.mobSpawner",
-            "craftableCreatures.mobSpawner", 2, 1, CCItems.SPAWN_EGG_BLUEPRINT, templateManager).registerStat();
-    public static final Achievement energy = new Achievement("achievement.craftableCreatures.energy", "craftableCreatures.energy", -2, -2,
-            CCBlocks.POWERED_BLUESTONE_BLOCK, bluedustCollector).registerStat();
-    public static final Achievement soulExtractor = new Achievement("achievement.craftableCreatures.soulExtractor", "craftableCreatures.soulExtractor",
-            -2, -4, CCBlocks.LIT_SOUL_EXTRACTOR, energy).registerStat();
-    public static final Achievement extractSoul = new Achievement("achievement.craftableCreatures.extractSoul", "craftableCreatures.extractSoul",
-            0, -4, CCItems.SOUL, soulExtractor).registerStat();
-    public static final Achievement combiner = new Achievement("achievement.craftableCreatures.combiner", "craftableCreatures.combiner",
-            -4, -2, CCBlocks.LIT_COMBINER, energy).registerStat();
-    public static final Achievement combineItem = new Achievement("achievement.craftableCreatures.combineItem", "craftableCreatures.combineItem",
-            -4, 0, Items.SPAWN_EGG, combiner).registerStat();
+            "craftableCreatures.mobSpawner", 2, 4, CCItems.SPAWN_EGG_BLUEPRINT, templateManager).registerStat();
+    public static final Achievement energy = new Achievement("achievement.craftableCreatures.energy",
+            "craftableCreatures.energy", 3, 0, CCBlocks.POWERED_BLUESTONE_BLOCK, bluedustCollector).registerStat();
+    public static final Achievement soulExtractor = new Achievement("achievement.craftableCreatures.soulExtractor",
+            "craftableCreatures.soulExtractor", 5, 2, CCBlocks.LIT_SOUL_EXTRACTOR, energy).registerStat();
+    public static final Achievement extractSoul = new Achievement("achievement.craftableCreatures.extractSoul",
+            "craftableCreatures.extractSoul", 7, 2, CCItems.SOUL, soulExtractor).registerStat();
+    public static final Achievement combiner = new Achievement("achievement.craftableCreatures.combiner",
+            "craftableCreatures.combiner", 5, -2, CCBlocks.LIT_COMBINER, energy).registerStat();
+    public static final Achievement combineItem = new Achievement("achievement.craftableCreatures.combineItem",
+            "craftableCreatures.combineItem", 7, -2, Items.SPAWN_EGG, combiner).registerStat();
 
     public static void register() {
         AchievementPage.registerAchievementPage(new AchievementPage(
