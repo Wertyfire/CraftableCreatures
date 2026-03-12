1407815864d3fb317f09736c9235bf9815446946
Wertyfire <sw405@yandex.ru>
2026-03-12 14:20:58 +0300

    Изменена система регистрации торгов

--- src/main/java/ru/wertyfiregames/craftablecreatures/common/CCTradeHandler.java
+++ src/main/java/ru/wertyfiregames/craftablecreatures/common/CCTradeHandler.java
@@ -7,29 +7,24 @@ package ru.wertyfiregames.craftablecreatures.common;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.village.MerchantRecipe;
 import net.minecraft.village.MerchantRecipeList;
-import net.minecraftforge.fml.relauncher.ReflectionHelper;
+import net.minecraftforge.fml.common.registry.VillagerRegistry;
 import ru.wertyfiregames.craftablecreatures.init.CCItems;
 import ru.wertyfiregames.craftablecreatures.item.EnumSoulElement;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class CCTradeHandler {
     public static void registerTrades() {
-        EntityVillager.ITradeList[][][][] allTrades = ReflectionHelper.getPrivateValue(
-                EntityVillager.class, null, "DEFAULT_TRADE_LIST_MAP", "field_175561_bA");
+        VillagerRegistry.VillagerProfession priest =
+                VillagerRegistry.instance().getRegistry().getValue(new ResourceLocation("priest"));
 
-        EntityVillager.ITradeList[][][] priestCareers = allTrades[2];
-        EntityVillager.ITradeList[][] clericLevels = priestCareers[0];
-        EntityVillager.ITradeList[] level2 = clericLevels[1];
+        if (priest == null) return;
 
-        EntityVillager.ITradeList[] newLevel2 = Arrays.copyOf(level2, level2.length + 1);
-
-        newLevel2[level2.length] = new SoulTrade();
-
-        clericLevels[1] = newLevel2;
+        VillagerRegistry.VillagerCareer cleric = priest.getCareer(0);
+        cleric.addTrade(2, new SoulTrade());
     }
 
     public static class SoulTrade implements EntityVillager.ITradeList {
@@ -38,7 +33,9 @@ public class CCTradeHandler {
             int meta = random.nextInt(EnumSoulElement.values().length);
             if (meta == 0) meta += 1 + random.nextInt(3);
 
-            recipeList.add(new MerchantRecipe(new ItemStack(CCItems.SOUL), new ItemStack(Items.EMERALD, 15), new ItemStack(CCItems.SOUL, 1, meta)));
+            recipeList.add(new MerchantRecipe(new ItemStack(CCItems.SOUL),
+                    new ItemStack(Items.EMERALD, 15),
+                    new ItemStack(CCItems.SOUL, 1, meta)));
         }
     }
 }
