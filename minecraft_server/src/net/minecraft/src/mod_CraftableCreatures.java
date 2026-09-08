9264956d18cde29973aa2a9a5c0d19e45881b550
Wertyfire <sw405@yandex.ru>
2026-09-08 21:24:59 +0300

    Обновлена версия мода

--- minecraft_server/src/net/minecraft/src/mod_CraftableCreatures.java
+++ minecraft_server/src/net/minecraft/src/mod_CraftableCreatures.java
@@ -17,8 +17,8 @@ import java.util.Random;
 import static net.minecraft.src.mod_CraftableCreatures.CraftableCreaturesIDs.*;
 
 public class mod_CraftableCreatures extends BaseModMp {
-    public static final String VERSION = "1.1.0";
-    public static final String BUILD = "03";
+    public static final String VERSION = "1.1.1";
+    public static final String BUILD = "04";
 
     public static mod_CraftableCreatures instance;
 
